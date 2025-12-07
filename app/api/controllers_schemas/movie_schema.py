278f1056de3c43e9868e89c72543320be59560ed
KasraMousapour <kasramousapour84@gmail.com>
2025-12-07 09:32:07 +0330

    refactor: change movie schemas

--- app/api/controllers_schemas/movie_schema.py
+++ app/api/controllers_schemas/movie_schema.py
@@ -1,24 +1,27 @@
-from pydantic import BaseModel
-from typing import Optional, List
+from pydantic import BaseModel, Field
+from typing import List
 from .director_schema import DirectorSchema
 from .genre_schema import GenreSchema
 
-class MovieBase(BaseModel):
-    title: str
-    release_year: Optional[int] = None
-    description: Optional[str] = None
-    director_id: Optional[int] = None
-    cast: Optional[str] = None
-    director: Optional[DirectorSchema]
-    genres: List[GenreSchema] = []
+class MovieCreate(BaseModel):
+    title: str = Field(..., min_length=1, max_length=200)
+    release_year: int = Field(..., ge=1888, le=2100)
+    description: str = Field(..., min_length=1)
+    director_id: int = Field(..., ge=1)
+    cast: str = Field(..., min_length=1)
+    genres: List[int] = Field(..., min_items=1)  # list of genre IDs
 
-class MovieCreate(MovieBase):
-    pass
-
-class MovieResponse(MovieBase):
+class MovieResponse(BaseModel):
     id: int
+    title: str
+    release_year: int
+    description: str
+    cast: str
     avg_rating: float
     rating_count: int
+    director_id: int
+    director: DirectorSchema
+    genres: List[int]
 
     class Config:
         orm_mode = True
